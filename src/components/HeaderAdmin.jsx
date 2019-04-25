3f51a496cadd474a0940ee7e412839f02e05b8a0
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-04-25 15:07:56 +0200

    editing application name

--- src/components/HeaderAdmin.jsx
+++ src/components/HeaderAdmin.jsx
@@ -14,9 +14,10 @@ class HeaderAdmin extends React.Component {
                 <Navbar bg="success" expand="lg">
 
                     <Navbar.Brand className="pb-0 pt-0">
-                        <img src="../images/logo.svg" className="d-inline-block align-top m-1" width="50" height="50" alt="logo" /> &nbsp;
-                        <span className="d-sm-inline d-none text-white pl-1 specialFont">Wasteaid</span>
-
+                        <Nav.Link className="pb-0 pt-0" href="/">
+                            <img src="../images/logo.svg" className="d-inline-block align-top m-1" width="50" height="50" alt="logo" /> &nbsp;
+                        <span className="d-sm-inline d-none text-white pl-1 specialFont">WasteAid</span>
+                        </Nav.Link>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
