c2bb0c0ca70b390b0479f2ba8b8274e69deba960
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-04-27 15:10:58 +0200

    'removed comments'

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -21,18 +21,6 @@ const AboutUs = props => {
                     <p><b>Frontend</b>: Javascript, React, Redux, Bootstrap, axios, OpenStreetMaps, Leaflet, Heroku.</p>
                     <p><b>Backend</b>: Node.js, Express, MongoDB, Mongoose, Morgan, Bcrypt, Jsonwebtoken, Cors.</p>
 
-
-                    {/* <div>We care about managing your waste and how you are managing your waste, the easy
-                                way. <br />
-                        Getting rid of your old wallpaper? Look up where it goes. Our app “Wasteaid” <br />
-                        comes in handy for just that.
-                                <br /><br />
-                        Technologies we used, got to work with, and learned about with in the process:
-                                <br /><br />
-                        Frontend: Javascript, React, Redux, Bootstrap, axios<br />
-                        Backend: Node.js, Express, MongoDB, Mongoose
-                                <br />
-                    </div> */}
                 </div>
 
             </div>
